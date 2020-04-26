957fec61bf6f6f691b51a48eba64f38e1661a44c
Alejo <alejobianchi@gmail.com>
2020-04-26 18:48:16 -0300

    update main.py const url web

--- main.py
+++ main.py
@@ -3,15 +3,9 @@ import os
 from modules.twitterApi.index import SendTwitter
 from modules.webScraper.index import ScrapProve
 
-from dotenv import load_dotenv
-from pathlib import Path  # python3 only
-
-env_path = Path('.') / '.env'
-load_dotenv(dotenv_path=env_path)
-
-autors_url = os.environ["AUTORS_URL"]
-base_url = os.environ["BASE_URL"]
-number_table_elements = int(os.environ["NUMBER_TABLE_ELEMENTS"])
+autors_url = "https://proverbia.net/autor/index"
+base_url = "https://proverbia.net"
+number_table_elements = 30
 
 my_scrapy = ScrapProve(base_url, autors_url, number_table_elements)
 li = my_scrapy.get_li_autor()
@@ -19,11 +13,11 @@ name_autor = li.a.string
 href_autor = li.a['href']
 quote_autor = my_scrapy.get_quote_autor(href_autor)
 
-# print(quote_autor + ' - ' + name_autor)
+# print(quote_autor + ' -' + name_autor)
 
 
-try:
-    SendTwitter(quote_autor + " -" + name_autor)
-    print("Twitte sending succefuli at: " + str(datetime.datetime.now()))
-except BaseException as error:
-    print('An exception occurred: {}'.format(error))
+# try:
+#     SendTwitter(quote_autor + " -" + name_autor)
+#     print("Twitte sending succefuli at: " + str(datetime.datetime.now()))
+# except BaseException as error:
+#     print('An exception occurred: {}'.format(error))
